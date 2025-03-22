cbb05a97896ed79d740650208e3d0f905b87bad5
Daniilpry <den291110@gmail.com>
2025-03-22 22:30:05 +0200

    :)

--- zd1.cpp
+++ zd1.cpp
@@ -3,7 +3,7 @@
 #include <stdint.h>
 
 int main() {
-    time_t max = (time_t)(~(time_t)0 >> 1); // Максимальне значення для знакового time_t
+    time_t max = (time_t)(~(time_t)0 >> 1);
     printf("Max time_t: %ld\n", (long)max);
     printf("End time_t: %s", ctime(&max));
     return 0;
