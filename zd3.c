e1987345e069b22fbdf20b854052585253795e81
Daniilpry <den291110@gmail.com>
2025-05-02 17:54:34 +0300

    fix zd3

--- zd3.c
+++ zd3.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int global_var;
-int global_init = 42;
+int global_var;               // BSS 
+int global_init = 42;         // Data
+
+void grow_stack() {
+    char big_array[10000];    
+    printf("New stack top after big_array: %p\n", &big_array);
+}
 
 int main() {
-    int local;
-    static int static_var;
-    static int static_init = 5;
-    int *heap = malloc(100);
+    int local;               
+    static int static_var;   
+    static int static_init = 5; 
+    int *heap = malloc(100); 
 
     printf("Stack top: %p\n", &local);
     printf("Global var: %p\n", &global_var);
@@ -17,6 +22,9 @@ int main() {
     printf("Static init: %p\n", &static_init);
     printf("Heap: %p\n", heap);
 
+    grow_stack();
+
     free(heap);
     return 0;
 }
+
