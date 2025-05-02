d162399a8c7ccde7e1ea2ad3b7a7364f57addcb8
Daniilpry <den291110@gmail.com>
2025-05-02 17:48:01 +0300

    add zd3

---
+++ zd3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int global_var;
+int global_init = 42;
+
+int main() {
+    int local;
+    static int static_var;
+    static int static_init = 5;
+    int *heap = malloc(100);
+
+    printf("Stack top: %p\n", &local);
+    printf("Global var: %p\n", &global_var);
+    printf("Global init: %p\n", &global_init);
+    printf("Static var: %p\n", &static_var);
+    printf("Static init: %p\n", &static_init);
+    printf("Heap: %p\n", heap);
+
+    free(heap);
+    return 0;
+}
